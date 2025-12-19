375f91d03619c5ef43bafc83d4eed24bf6108e6e
ethyashpathak <ethyashpathak@gmail.com>
2025-12-20 01:07:50 +0530

    Fix import path for Frameworks component in About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/Globe.jsx";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "../components/Frameworks.jsx";
 
 
 const About = () => {
